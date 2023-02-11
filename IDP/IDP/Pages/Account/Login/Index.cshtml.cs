368c64dea60ed7aa2c5f5486c05f4bce508b4177
Kushtrim <kmu@mca.dev>
2023-02-11 16:29:05 +0100

    Remove editorconfig for IDP solution

--- IDP/IDP/Pages/Account/Login/Index.cshtml.cs
+++ IDP/IDP/Pages/Account/Login/Index.cshtml.cs
@@ -3,9 +3,9 @@ using Duende.IdentityServer.Events;
 using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
 using Duende.IdentityServer.Stores;
-using Duende.IdentityServer.Test;
-using IDP.Services;
+
 using IDP.Services.Interfaces;
+
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
